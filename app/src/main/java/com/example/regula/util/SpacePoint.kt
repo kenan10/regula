736beb3db6b6d477926435821d386f5830150a28
Your Name <you@example.com>
2022-12-15 15:42:49 +0200

    add comments for formulas implementation

--- app/src/main/java/com/example/regula/util/SpacePoint.kt
+++ app/src/main/java/com/example/regula/util/SpacePoint.kt
@@ -6,6 +6,11 @@ import kotlin.math.sqrt
 
 class SpacePoint(val accelerometerAngle: Float, val magnetometerAngle: Float) {
     companion object Factory {
+        /**
+         * Factory method to create [SpacePoint] from accelerometer and magnetometer sensors
+         * values
+         * @return Newly created SpacePoint
+         */
         fun fromSensorsValues(
             accelerometerValue: List<Float>, magnetometerValue: List<Float>
         ): SpacePoint {
@@ -23,13 +28,22 @@ class SpacePoint(val accelerometerAngle: Float, val magnetometerAngle: Float) {
         }
     }
 
+    /**
+     * Used to check is instance in circle with another [SpacePoint] as center
+     *
+     * @param deviation radius of circle
+     * @param center point which will be used as a center
+     * @return whether true or false if instance is in circle or not
+     */
     fun isInCircle(deviation: Float, center: SpacePoint): Boolean {
-        return deviation > (this.magnetometerAngle - center.magnetometerAngle).pow(2) + (this.accelerometerAngle - center.accelerometerAngle).pow(
+        return deviation > ((this.magnetometerAngle - center.magnetometerAngle) / 10).pow(2) + ((this.accelerometerAngle - center.accelerometerAngle) / 45).pow(
             2
         )
     }
 
     fun distanceTo(center: SpacePoint): Float {
-        return (this.magnetometerAngle - center.magnetometerAngle).pow(2) + (this.accelerometerAngle - center.accelerometerAngle).pow(2)
+        return ((this.magnetometerAngle - center.magnetometerAngle) / 10).pow(2) + ((this.accelerometerAngle - center.accelerometerAngle) / 45).pow(
+            2
+        )
     }
 }
